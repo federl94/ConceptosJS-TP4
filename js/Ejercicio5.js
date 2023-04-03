c8aaca3e83de954f23ef5bd231ea9c3c818772cc
Federico <federuedaleal@hotmail.com>
2023-04-03 00:16:54 -0300

    Ejericicio 5 (da error, falta corregir)

---
+++ js/Ejercicio5.js
@@ -0,0 +1,104 @@
+/*5- Crea una clase llamada Persona que siga las siguientes condiciones:
+Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
+Los métodos que se debe poder utilizar  son:
+mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación.
+esMayorDeEdad: indica si es mayor de edad, devuelve un mensaje indicando que la persona es mayor de edad.
+mostrarDatos: devuelve toda la información del objeto.
+generaDNI(): genera un número aleatorio de 8 cifras.*/
+class Persona {
+    constructor(nombre, edad, dni, sexo, peso, altura,anioNac){
+        this.nombre = nombre
+        this.edad = edad
+        this.dni = dni || this.generarDNI()
+        this.sexo = sexo
+        this.peso = peso
+        this.altura = altura
+        this.anioNac = anioNac
+    }
+    get nombre (){
+        return this.nombre
+    }
+    get edad(){
+        return this.edad
+    }
+    get dni (){
+        return this.dni
+    }
+    get sexo (){
+        return this.sexo
+    }
+    get peso (){
+        return this.peso
+    }
+    get altura (){
+        return this.altura
+    }
+    get anioNac(){
+        return this.anioNac
+    }
+    set nombre(nuevoNombre){
+        if(nuevoNombre.lenght >=50){
+            return this.nombre = nuevoNombre;
+        }       
+    }
+    set edad(nuevaEdad){
+        if (nuevaEdad > 0 && nuevaEdad < 130) {
+        return this.edad = nuevaEdad;
+        }
+    }
+    set dni(nuevoDni){
+        return this.dni = nuevoDni;
+    }
+    set sexo(nuevoSexo){
+        if (nuevoSexo == `Hombre` || nuevoSexo == `Mujer` || nuevoSexo == `No definido`){
+            return this.sexo = nuevoSexo;
+        }
+    }
+    set peso (nuevoPeso){
+        return this.peso = nuevoPeso;
+    }
+    set altura(nuevaAltura){
+        return this.altura = nuevaAltura;
+    }
+    set anioNac(nuevoAnioNac){
+        if (nuevoAnioNac>=1930 && nuevoAnioNac<=2023){
+            return this.anioNac = nuevoAnioNac;
+        }
+    }
+    mostrarGeneracion(){
+        if (this.anio >= 1994 && this.anio <= 2010) {
+            document.write("<br>Es de la generacion: Z");
+            document.write("<br> Rasgo caracteristico: Irreverencia");
+        } else if (this.anio >= 1981 && this.anio <= 1993) {
+            document.write("<br>Es de la generacion: millnials Y");
+            document.write("<br> Rasgo caracteristico: Frustracion");
+        } else if (this.anio >= 1969 && this.anio <= 1980) {
+            document.write("<br>Es de la generacion: X");
+            document.write("<br> Rasgo caracteristico: Obsesion por el Exito");
+        } else if (this.anio >= 1949 && this.anio <= 1968) {
+            document.write("<br>Es de la generacion: Baby Boom");
+            document.write("<br> Rasgo caracteristico: Ambicion");
+        } else {
+            document.write("<br>Es de la generacion: Silent Generation (Los niños de la posguerra)");
+            document.write("<br> Rasgo caracteristico: Austeridad");
+        }
+    }
+    esMayorDeEdad() {
+        if (this.edad >= 18) {
+            document.write(`<br>Es mayor de edad`);
+        } else {
+            document.write(`<br>No es mayor de edad`);
+        }
+    }
+    generarDNI() {
+        let dni = Math.floor(Math.random() * (99999999 - 10000000) + 10000000);
+        return dni;
+    }
+    mostrarDatos(){
+        document.write(`Nombre: ${this.nombre}<br>Edad: ${this.edad}<br>DNI: ${this.dni}<br>Sexo: ${this.sexo}<br>Peso: ${this.peso}<br>Altura: ${this.altura}<br>Año de Nacimiento: ${this.anioNac}<br>`)
+    }
+    }
+    const federico = new Persona ("Federico", 29, "Hombre", 88, 170,1994,34513548);
+    federico.esMayorDeEdad();
+    federico.mostrarGeneracion();
+    federico.mostrarDatos();
